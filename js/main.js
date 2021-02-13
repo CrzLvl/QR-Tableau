1ee3ad89de9c6bc5cd07b1f25fafcc306c32ab32
CrzLvl <51365423+CrzLvl@users.noreply.github.com>
2021-02-14 09:43:09 +1000

    d3 test

---
+++ js/main.js
@@ -0,0 +1,13 @@
+import {select, csv} from 'd3'
+
+const svg = select('svg');
+
+const width = +svg.attr('width');
+const height = +svg.attr('height');
+
+csv('data.csv').then(data => {
+    data.forEach(d => {
+        d.population = +d.population*1000;
+    });
+    console.log(data);
+})

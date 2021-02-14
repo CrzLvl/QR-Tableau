f9faed436a8f05a823081420a07f1b21a98ee109
CrzLvl <51365423+CrzLvl@users.noreply.github.com>
2021-02-14 20:55:31 +1000

    Added additional content
    New graph using Vega-Lite, added descriptions, tweaked CSS.

--- js/main.js
+++ js/main.js
@@ -1,13 +1,46 @@
-import {select, csv} from './d3.min.js'
-
-const svg = select('svg');
-
-const width = +svg.attr('width');
-const height = +svg.attr('height');
-
-csv('data.csv').then(data => {
-    data.forEach(d => {
-        d.population = +d.population*1000;
-    });
-    console.log(data);
-})
+var patronageGraph = {
+    "$schema": "https://vega.github.io/schema/vega-lite/v4.json",
+    "title": { text: "TravelTrain Patronage", fontSize: 26, titleFont: "Montserrat" },
+    "description": "Quarterly TravelTrain patronage.",
+    "width": "container",
+    "height": "container",
+    "data": { "url": "js/qrttp.json" },
+    "layer": [
+        {
+            "mark": {
+                "type": "line",
+                "point": {
+                    "filled": false,
+                    "fill": "white"
+                },
+                "interpolate": "natural"
+            },
+            "encoding": {
+                "x": { "field": "Date", "type": "temporal", axis: { tickCount: 10, titleFontSize: 20, labelFontSize: 15 } },
+                "y": { "field": "Patrons", "type": "quantitative", axis: { titleFontSize: 20, labelFontSize: 15 } },
+                "color": { "value": "#C3262F" },
+                "tooltip": [
+                    { "title": "Patrons", "field": "Patrons", "type": "quantitative" }
+                ]
+            }
+        }, {
+            "transform": [
+                {
+                    "regression": "Date",
+                    "on": "Patrons"
+                }
+            ],
+            "mark": {
+                "type": "line",
+                "strokeDash": [10, 6],
+                "interpolate": "natural",
+                "opacity": 0.5
+            },
+            "encoding": {
+                "x": { "field": "Date", "type": "temporal", axis: { tickCount: 10, titleFontSize: 20, labelFontSize: 15 } },
+                "y": { "field": "Patrons", "type": "quantitative", axis: { titleFontSize: 20, labelFontSize: 15 } },
+                "color": { "value": "#505050" }
+            }
+        }]
+}
+vegaEmbed('#patronageGraph', patronageGraph);

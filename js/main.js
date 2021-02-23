8113af6057e1fee40d8aa4da91dda61d71bbf24a
CrzLvl <51365423+CrzLvl@users.noreply.github.com>
2021-02-23 11:24:28 +1000

    Added interactive network map

--- js/main.js
+++ js/main.js
@@ -44,3 +44,277 @@ var patronageGraph = {
         }]
 }
 vegaEmbed('#patronageGraph', patronageGraph);
+
+const cssOpacity = {opacity: '1', transition: 'opacity 0.2s ease, fill 0.2s ease'}
+const cssFaded = {opacity: '0.1'}
+
+$(document).ready(function(){
+    qrMapInit()
+})
+
+function qrMapInit() {
+    var m = document.getElementById("qrMap");
+
+    m.addEventListener("load",function(){
+        var svgDoc = m.contentDocument.documentElement;
+
+        line = $(".jsLine",svgDoc);
+        linePath = $(line).children(".lineStroke")
+        station = $(".jsStn",svgDoc);
+        stationCommon = $(".commonStn",svgDoc);
+        stationLabel = $(".jsStnLbl",svgDoc);
+        lineLabel = $(".jsLineLbl",svgDoc);
+        _spacers = $("#_x5F_SPACER",svgDoc);
+
+        $(line).css(cssOpacity)
+        $(linePath).css(cssOpacity)
+        $(station).css(cssOpacity)
+        $(stationCommon).css(cssOpacity)
+        $(stationLabel).css(cssOpacity)
+        $(lineLabel).css(cssOpacity)
+        $(_spacers).css(cssOpacity)
+
+        qrMapPrep()
+    },false);
+}
+
+function qrMapPrep() {
+    // Mouseover
+    // Lines
+    linePath.mouseover(function () {
+        highlight(sharedLines($(this).attr('id').replace(/Path/g, 'Line')),null)
+    })
+    linePath.mouseout(function () {
+        highlightClear()
+    })
+
+    // Line labels
+    lineLabel.mouseover(function () {
+        highlight(sharedLines($(this).attr('id').replace('Label','')),null)
+    })
+    lineLabel.mouseout(function () {
+        highlightClear()
+    })
+
+    // Stations
+    station.mouseover(function () {
+        s = $(this).attr('id').replace(/S-/g, '') // Handle Springfield Central station IDs
+        highlight(null,s)
+    })
+    station.mouseout(function () {
+        highlightClear()
+    })
+
+    // Station labels
+    stationLabel.mouseover(function () {
+        s = $(this).attr('id').replace(/Label/g, 'Stn')
+        s = s.replace(/S-/g, '') // Handle Springfield Central station IDs
+        highlight(null,s)
+
+        $(this).css({fill: "#C3262F"})
+    })
+    stationLabel.mouseout(function () {
+        highlightClear()
+        $(this).css({fill: "#000000"})
+    })
+}
+
+function highlight(l, s) {
+    // l = line, s = station,
+    if (l && !s) {
+       highlightLine(l)
+    } else if (s && !l) {
+        var test = getLines(s)
+        highlightLine(test)
+    }
+
+}
+
+function highlightLine(l) {
+    let b = ''
+    let c = ''
+    let d = false
+    let stationList = []
+    let newStations = []
+    let g = ''
+    let h = ''
+    let j = ''
+    let k = ''
+    l.forEach(el => {
+        a = "#" + el + ","
+        b += a
+
+        f = "#" + el + "Label,"
+        c += f
+        
+        newStations = getStations(el)
+        if (newStations) {
+            newStations.forEach(el => {
+                stationList.push(el)
+            });
+        }
+
+        if (el == "Sunshine_CoastLine") {
+            c+= "#Nambour-Caboolture_RailbusLineLabel,"
+        }
+    });
+    b = b.slice(0, -1)
+    c = c.slice(0, -1)
+
+    stationList = Array.from(new Set(stationList))
+
+    stationList.forEach(el => {
+        g = "#" + el + "Stn,"
+        h += g.replace(/ /g,'_')
+
+        j = "#" + el + "Label,"
+        k += j.replace(/ /g,'_')
+    })  
+    h = h.slice(0, -1)          
+    k = k.slice(0, -1)          
+
+    line.not(b).css(cssFaded)
+    lineLabel.not(c).css(cssFaded)
+    stationCommon.not(h).css(cssFaded)
+    stationLabel.not(k).css(cssFaded)
+
+    if (b.includes("Rosewood")) {
+        $('#IpswichStnRswd',document.getElementById("qrMap").contentDocument.documentElement).css({opacity: '1'})
+    } else {
+        $('#IpswichStnRswd',document.getElementById("qrMap").contentDocument.documentElement).css({opacity: '0'}) 
+    }
+
+    d = spacerHandler(b)
+    if (d) {
+        $(_spacers).css('opacity', '0')
+    }
+}
+
+function highlightClear() {
+    $(line).css({opacity: '1'})
+    $(station).css({opacity: '1'})
+    $(stationLabel).css({opacity: '1'})
+    $(lineLabel).css({opacity: '1'})
+    $(_spacers).css({opacity: '1'})
+}
+
+// Check which stations a line contains
+function getStations(l) {
+    l = l.replace('_', ' ').replace('Line','')
+    try {
+        return lineData[l].stations
+    } catch (error) {} 
+}
+
+// Check which lines a station is on
+function getLines(s) {
+    s = s.replace(/Stn/g, '')
+    s = s.replace(/_/g, ' ')
+    stationArray = [];
+    $.each(lineData, function(i, v) {
+        if(v.stations.includes(s)) {
+            stationArray.push(i.replace(/ /g, '_') + "Line")
+        }
+    }) 
+    
+    getLines_LineArray = []
+
+    $.each(stationArray, function(i, l) {
+        getLines_TempLines = sharedLines(l)
+        $.each(getLines_TempLines, function(i, l) {
+            getLines_LineArray.push(l)
+        })
+
+    })
+
+    stationArray = Array.from(new Set(getLines_LineArray))
+    return stationArray;
+}
+
+// Handle shared line colours
+function sharedLines(a) {
+    switch (a) {
+        case "GreenShared":
+            return ["Sunshine_CoastLine", "CabooltureLine", "GreenShared", "IpswichLine", "IpRwShared", "RosewoodLine", "RailbusLine"]
+            break;
+    
+        case "LightBlueShared":
+            return ["Redcliffe_PeninsulaLine", "LightBlueShared", "Springfield_CentralLine"]
+            break;
+
+        case "DarkBlueShared":
+            return ["ClevelandLine", "DarkBlueShared", "ShorncliffeLine"]
+            break;
+    
+        case "YellowShared":
+            return ["AirportLine", "YellowShared", "Gold_CoastLine"]
+            break;
+    
+        case "RedShared":
+            return ["BeenleighLine", "RedShared", "Ferny_GroveLine"]
+            break;
+    
+        case "IpRwShared":
+            return [a, "IpswichLine", "RosewoodLine", "GreenShared"]
+            break;
+    
+        case "AirportLine":
+        case "Gold_CoastLine":
+            return [a, "YellowShared"]
+            break;
+    
+        case "Sunshine_CoastLine":
+            return [a, "CabooltureLine", "GreenShared", "RailbusLine"]
+            break;
+
+        case "RosewoodLine":
+            return [a, "IpRwShared"]
+            break;
+
+        case "CabooltureLine":
+        case "IpswichLine":
+            return [a, "GreenShared", "IpRwShared"]
+            break;
+
+        case "Redcliffe_PeninsulaLine":
+        case "Springfield_CentralLine":
+            return [a, "LightBlueShared"]
+            break;
+
+        case "ShorncliffeLine":
+        case "ClevelandLine":
+            return [a, "DarkBlueShared"]
+            break;
+
+        case "BeenleighLine":
+        case "Ferny_GroveLine":
+            return [a, "RedShared"]
+            break;
+
+        case "RailbusLine":
+            return [a, "Sunshine_CoastLine"]
+            break;
+    
+        default:
+            return [a]
+            break;
+    }
+}
+
+// Ipswich line spacer handler
+function spacerHandler(l) {
+    if (l.includes('IpswichLine') && !l.includes('Springfield_CentralLine')) {
+            d = true
+        } else {
+           d = false
+        }
+    return d
+}
+
+document.getElementById('qrMap').addEventListener('load', function () {
+    svgPanZoom(document.getElementById('qrMap'), {
+        minZoom: 1,
+        maxZoom: 7.5,
+        controlIconsEnabled: true,
+    });
+});
---
+++ js/qrMapData.js
@@ -0,0 +1,58 @@
+let lineData = {
+        "Airport": {
+            "stations": ["Roma Street", "Brisbane Central", "Fortitude Valley", "Bowen Hills", "Albion", "Wooloowin", "Eagle Junction", "Airport International", "Airport Domestic"],
+            "lineSegments": ["AirportLine", "YellowShared"]
+        },
+        "Beenleigh": {
+            "stations": ["Bowen Hills", "Fortitude Valley", "Brisbane Central", "Roma Street", "South Brisbane", "South Bank", "Park Road", "Dutton Park", "Fairfield", "Yeronga", "Yeerongpilly", "Moorooka", "Rocklea", "Salisbury", "Coopers Plains", "Banoon", "Sunnybank", "Altandi", "Runcorn", "Fruitgrove", "Kuraby", "Trinder Park", "Woodridge", "Kingston", "Loganlea", "Bethania", "Edens Landing", "Holmview", "Beenleigh"],
+            "lineSegments": ["BeenleighLine", "RedShared"]
+        },
+        "Caboolture": {
+            "stations": ["Roma Street", "Brisbane Central", "Fortitude Valley", "Bowen Hills", "Eagle Junction", "Northgate", "Petrie", "Dakabin", "Narangba", "Burpengary", "Morayfield", "Caboolture"],
+            "lineSegments": ["CabooltureLine", "GreenShared"]
+        },
+        "Cleveland": {
+            "stations": ["Bowen Hills", "Fortitude Valley", "Brisbane Central", "Roma Street", "South Brisbane", "South Bank", "Park Road", "Buranda", "Coorparoo", "Norman Park", "Morningside", "Cannon Hill", "Murarrie", "Hemmant", "Lindum", "Wynnum North", "Wynnum", "Wynnum Central", "Manly", "Lota", "Thorneside", "Birkdale", "Wellington Point", "Ormiston", "Cleveland"],
+            "lineSegments": ["ClevelandLine", "DarkBlueShared"]
+        },
+        "Doomben": {
+            "stations": ["Roma Street", "Brisbane Central", "Fortitude Valley", "Bowen Hills", "Albion", "Wooloowin", "Eagle Junction", "Clayfield", "Hendra", "Ascot", "Doomben"],
+            "lineSegments": ["DoombenLine"]
+        },
+        "Exhibition": {
+            "stations": ["Roma Street", "Brisbane Central", "Fortitude Valley", "Exhibition"],
+            "lineSegments": ["ExhibitionLine"]
+        },
+        "Ferny Grove": {
+            "stations": ["Roma Street", "Brisbane Central", "Fortitude Valley", "Bowen Hills", "Windsor", "Wilston", "Newmarket", "Alderley", "Enoggera", "Gaythorne", "Mitchelton", "Oxford Park", "Grovely", "Keperra", "Ferny Grove"],
+            "lineSegments": ["Ferny_GroveLine", "RedShared"]
+        },
+        "Gold Coast": {
+            "stations": ["Bowen Hills", "Fortitude Valley", "Brisbane Central", "Roma Street", "South Brisbane", "South Bank", "Park Road", "Altandi", "Loganlea", "Beenleigh", "Ormeau", "Coomera", "Helensvale", "Nerang", "Robina", "Varsity Lakes"],
+            "lineSegments": ["Gold_CoastLine", "YellowShared"]
+        },
+        "Ipswich": {
+            "stations": ["Bowen Hills", "Fortitude Valley", "Brisbane Central", "Roma Street", "Milton", "Auchenflower", "Toowong", "Taringa", "Indooroopilly", "Chelmer", "Graceville", "Sherwood", "Corinda", "Oxley", "Darra", "Wacol", "Gailes", "Goodna", "Redbank", "Riverview", "Dinmore", "Ebbw Vale", "Bundamba", "Booval", "East Ipswich", "Ipswich"],
+            "lineSegments": ["IpswichLine", "GreenShared"]
+        },
+        "Redcliffe Peninsula": {
+            "stations": ["Roma Street", "Brisbane Central", "Fortitude Valley", "Bowen Hills", "Eagle Junction", "Northgate", "Virginia", "Sunshine", "Geebung", "Zillmere", "Carseldine", "Bald Hills", "Strathpine", "Bray Park", "Lawnton", "Petrie", "Kallangur", "Murrumba Downs", "Mango Hill", "Mango Hill East", "Rothwell", "Kippa-Ring"],
+            "lineSegments": ["Redcliffe_PeninsulaLine", "LightBlueShared"]
+        },
+        "Rosewood": {
+            "stations": ["Ipswich", "Thomas Street", "Wulkuraka", "Karrabin", "Walloon", "Thagoona", "Rosewood"],
+            "lineSegments": ["RosewoodLine"]
+        },
+        "Shorncliffe": {
+            "stations": ["Roma Street", "Brisbane Central", "Fortitude Valley", "Bowen Hills", "Albion", "Wooloowin", "Eagle Junction", "Toombul", "Nundah", "Northgate", "Bindha", "Banyo", "Nudgee", "Boondall", "North Boondall", "Deagon", "Sandgate", "Shorncliffe"],
+            "lineSegments": ["ShorncliffeLine", "DarkBlueShared"]
+        },
+        "Springfield Central": {
+            "stations": ["Bowen Hills", "Fortitude Valley", "Brisbane Central", "Roma Street", "Milton", "Auchenflower", "Toowong", "Taringa", "Indooroopilly", "Chelmer", "Graceville", "Sherwood", "Corinda", "Oxley", "Darra", "Richlands", "Springfield", "Springfield Central"],
+            "lineSegments": ["SpringfieldLine", "LightBlueShared"]
+        },
+        "Sunshine Coast": {
+            "stations": ["Roma Street", "Brisbane Central", "Fortitude Valley", "Bowen Hills", "Eagle Junction", "Northgate", "Petrie", "Dakabin", "Narangba", "Burpengary", "Morayfield", "Caboolture", "Elimbah", "Beerburrum", "Glasshouse Mountains", "Beerwah", "Landsborough", "Mooloolah", "Eudlo", "Palmwoods", "Woombye", "Nambour", "Yandina", "Eumundi", "Cooroy", "Pomona", "Cooran", "Traveston", "Gympie North"],
+            "lineSegments": ["Sunshine_CoastLine", "GreenShared"]
+        }
+    }
